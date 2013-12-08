a00a27a82ac44e10197e92d2d93dce9ca1ccb339
Dmitriy Volik <dvolik@ebay.com>
2013-12-08 21:48:01 +0200

    wysiwyg styles

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -9,27 +9,40 @@ CKEDITOR.editorConfig = function( config ) {
     // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
     // The toolbar groups arrangement, optimized for two toolbar rows.
-    config.toolbarGroups = [
-        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-        { name: 'editing',     groups: [ 'find', 'selection'] },
-        { name: 'links' },
-        { name: 'insert' },
-        { name: 'forms' },
-        { name: 'tools' },
-        { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-        { name: 'others' },
-        '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
-        { name: 'styles' },
-        { name: 'colors' },
-    ];
+//    config.toolbarGroups = [
+//        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+//        { name: 'editing',     groups: [ 'find', 'selection'] },
+//        { name: 'links' },
+//        { name: 'insert' },
+//        { name: 'forms' },
+//        { name: 'tools' },
+//        { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+//        { name: 'others' },
+//        '/',
+//        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+//        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+//        { name: 'styles' },
+//        { name: 'colors' },
+//    ];
 
+
+    config.toolbar =
+        [
+            { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Subscript','Superscript','-','RemoveFormat' ] },
+            { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+                '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+            { name: 'clipboard', items : ['PasteText','PasteFromWord' ] },
+            { name: 'insert', items : ['Table','HorizontalRule','SpecialChar' ] },
+            { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+            { name: 'colors', items : [ 'TextColor','BGColor' ] },
+
+        ];
     // Remove some buttons, provided by the standard plugins, which we don't
     // need to have in the Standard(s) toolbar.
     config.removeButtons = 'Underline,Subscript,Superscript';
     config.language = 'ru';
-    //config.contentsCss = '/css/editor.css';
+    config.height = '600px';
+    config.contentsCss = '/css/editor.css';
 
     config.extraPlugins = 'typograf';
 };
